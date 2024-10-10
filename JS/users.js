6827e6d1d4924ebea57659e322efac30d1730305
Dhyey <dhyeypf@gmail.com>
2024-10-10 14:16:05 +0530

    finish search bar functionality

--- JS/users.js
+++ JS/users.js
@@ -1,12 +1,39 @@
 
 const searchBar = document.querySelector(".users .search input");
 const searchBtn = document.querySelector(".users .search button");
+const usersList = document.querySelector(".users .users-list");
+
 
 searchBtn.onclick = () => {
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
+    searchBar.value = "";
 }
+
+
+searchBar.onkeyup = () => {
+    let searchTerm = searchBar.value;
+    if (searchTerm != "") {
+        searchBar.classList.add("active");
+    } else {
+        searchBar.classList.remove("active");
+    }
+    // Start AJEX
+    const xhr = new XMLHttpRequest();  // Create XML object
+    xhr.open("POST", "php/search.php", true);
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+}
+
 
 setInterval(() => {
     // Start AJEX
@@ -16,7 +43,9 @@ setInterval(() => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 let data = xhr.response;
-                console.log(data);
+                if (!searchBar.classList.contains("active")) {
+                    usersList.innerHTML = data;
+                }
             }
         }
     }
--- users.php
+++ users.php
@@ -34,56 +34,8 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="assets/avatar12.jpg" alt="">
-                        <div class="details">
-                            <span>Amy</span>
-                            <p>This is the test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot online"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="assets/avatar13.jpg" alt="">
-                        <div class="details">
-                            <span>Jake</span>
-                            <p>This is the test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot online"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="assets/avatar14.jpg" alt=""> 
-                        <div class="details">
-                            <span>Kate</span>
-                            <p>This is the test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot online"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="assets/avatar15.jpg" alt="">
-                        <div class="details">
-                            <span>Tom</span>
-                            <p>This is the test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot online"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="assets/avatar17.jpg" alt="">
-                        <div class="details">
-                            <span>Elle</span>
-                            <p>This is the test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot online"><i class="fas fa-circle"></i></div>
-                </a>
+               
+                
             </div>
         </section>
     </div>
